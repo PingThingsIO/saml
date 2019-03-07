3f57c9fa1076c9d9ca0d8c5face0732c120f7a25
Pierre Gourlaouen <pi.gourlaouen@gmail.com>
2019-03-07 18:32:33 +0100

    Add log

--- service_provider.go
+++ service_provider.go
@@ -602,9 +602,11 @@ func (sp *ServiceProvider) validateSigned(responseEl *etree.Element) error {
 		return err
 	}
 	if sigEl != nil {
+		fmt.Println("in first signature")
 		if err = sp.validateSignature(responseEl); err != nil {
 			return fmt.Errorf("cannot validate signature on Response: %v", err)
 		}
+		fmt.Println("has first signature")
 		haveSignature = true
 	}
 
@@ -613,14 +615,17 @@ func (sp *ServiceProvider) validateSigned(responseEl *etree.Element) error {
 		return err
 	}
 	if assertionEl != nil {
+	fmt.Println("in second assertation")
 		sigEl, err := findChild(assertionEl, "http://www.w3.org/2000/09/xmldsig#", "Signature")
 		if err != nil {
 			return err
 		}
 		if sigEl != nil {
+		fmt.Println("in second signature")
 			if err = sp.validateSignature(assertionEl); err != nil {
 				return fmt.Errorf("cannot validate signature on Response: %v", err)
 			}
+			fmt.Println("has second signature")
 			haveSignature = true
 		}
 	}
@@ -637,6 +642,10 @@ func (sp *ServiceProvider) validateSignature(el *etree.Element) error {
 	if err != nil {
 		return err
 	}
+	
+	fmt.Println("after idp")
+
+	fmt.Println(cert)
 
 	certificateStore := dsig.MemoryX509CertificateStore{
 		Roots: []*x509.Certificate{cert},
@@ -676,6 +685,14 @@ func (sp *ServiceProvider) validateSignature(el *etree.Element) error {
 	el, err = etreeutils.NSDetatch(ctx, el)
 	if err != nil {
 		return err
+	}
+	
+	fmt.Println(el.Text())
+	roots, err := certificateStore.Certificates()
+	fmt.Println(roots)
+
+	for _, root := range roots {
+		fmt.Println(root.Raw)
 	}
 
 	_, err = validationContext.Validate(el)
