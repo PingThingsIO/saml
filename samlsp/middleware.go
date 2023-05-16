561bfc3cb7863e0c575d90483ac96f44b71e0c1e
pdemange <paul@pingthings.io>
2023-05-16 15:58:32 -0700

    Dump cookies

--- samlsp/middleware.go
+++ samlsp/middleware.go
@@ -3,6 +3,7 @@ package samlsp
 import (
 	"bytes"
 	"encoding/xml"
+	"log"
 	"net/http"
 
 	"github.com/crewjam/saml"
@@ -114,7 +115,11 @@ func (m *Middleware) ServeACS(w http.ResponseWriter, r *http.Request) {
 // to start the SAML auth flow.
 func (m *Middleware) RequireAccount(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		panic("Panic in RequireAccount!")
+		cookies := r.Cookies()
+		for _, cookie := range cookies {
+			log.Printf("Cookie Name: %s\n", cookie.Name)
+			log.Printf("Cookie Value: %s\n", cookie.Value)
+		}
 		session, err := m.Session.GetSession(r)
 		if session != nil {
 			r = r.WithContext(ContextWithSession(r.Context(), session))
